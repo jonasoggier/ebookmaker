c658ffb19fb0a62e9838c21cb18f697128ad8f7e
Jonas Oggier <jonas.oggier@gmail.com>
2014-06-16 22:00:00 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebookmaker::Application.config.secret_key_base = '49906c72e0f56a870675e63e2bf73e476965f344c8f7fcbb1a2838f4a3614f8bb7a1802a97580dee60c72b2ff84c50cddfcabab0c6749535bd6fe6283f4838ed'
